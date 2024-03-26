5d4c4317d71b74b3d35423a161d2a13ea8ab7129
Rafael-LN <rafaelnunes3092@gmail.com>
2024-03-26 21:59:17 +0000

    Add ClientHandler class

---
+++ client/ClientHandler.java
@@ -0,0 +1,30 @@
+package client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.List;
+
+public class ClientHandler implements Runnable {
+    private Socket clientSocket;
+    private List<ClientHandler> clients;
+
+    public ClientHandler(Socket clientSocket, List<ClientHandler> clients) {
+        this.clientSocket = clientSocket;
+        this.clients = clients;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream inputStream = clientSocket.getInputStream();
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            // Lógica de comunicação com o cliente (leitura e escrita)
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
