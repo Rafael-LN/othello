0383b9cdab0c5730be63182de6c379608b7510c6
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-09 17:44:56 +0100

    Refactor code
    * Notify client only when player registration is successfully

--- src/client/ClientHandler.java
+++ src/client/ClientHandler.java
@@ -1,5 +1,6 @@
 package src.client;
 
+import src.io.PlayerXmlHandler;
 import src.model.Player;
 
 import java.io.IOException;
@@ -11,10 +12,12 @@ import java.util.List;
 public class ClientHandler implements Runnable {
     private Socket clientSocket;
     private List<ClientHandler> clients;
+    private PlayerXmlHandler playerXmlHandler;
 
     public ClientHandler(Socket clientSocket, List<ClientHandler> clients) {
         this.clientSocket = clientSocket;
         this.clients = clients;
+        this.playerXmlHandler = new PlayerXmlHandler();
     }
 
     @Override
@@ -23,26 +26,48 @@ public class ClientHandler implements Runnable {
             ObjectInputStream objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());
 
-            objectOutputStream.writeObject("Please provide player information: nickname, password, nationality, age, and photo URL");
-            objectOutputStream.flush();
+            // Handle player registration
+            Player player = registerPlayer(objectInputStream, objectOutputStream);
 
+            // Handle lobby interactions
+            handleLobbyInteraction(player, objectInputStream, objectOutputStream);
 
-            Player player = (Player) objectInputStream.readObject();
-
-            objectOutputStream.writeObject("Received registration: " + player);
-            objectOutputStream.flush();
-
-            System.out.println("Received registration: " + player);
 
             objectOutputStream.close();
             objectInputStream.close();
             clientSocket.close();
             clients.remove(this);
 
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
         }
+    }
+
+    private Player registerPlayer(ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream) throws IOException, ClassNotFoundException {
+        // Prompt player to provide information for registration
+        objectOutputStream.writeObject("Please provide player information: nickname, password, nationality, age, and photo URL");
+        objectOutputStream.flush();
+
+        // Receive player information from client
+        Player player = (Player) objectInputStream.readObject();
+
+        // Simulate registration process (e.g., store player data in database)
+        // Example: PlayerDatabase.register(player);
+        if (playerXmlHandler.registerPlayer(player)){
+            // Notify client that registration is successful
+            objectOutputStream.writeObject("Registration successful. Welcome, " + player.getNickname() + "!");
+            objectOutputStream.flush();
+
+            System.out.println("Received registration: " + player);
+        }
+
+
+        return player;
+    }
+
+    //TODO
+    private void handleLobbyInteraction(Player player, ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream) throws IOException {
+        // Implement logic to handle lobby interactions
+        // Example: player joining lobby, leaving lobby, etc.
     }
 }
