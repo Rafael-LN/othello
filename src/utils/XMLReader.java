5b95e1b3c245c47432013f74ecf4db8ce03c96dc
Rafael-LN <rafaelnunes3092@gmail.com>
2024-05-14 20:44:00 +0100

    Create XMLReader class

---
+++ src/utils/XMLReader.java
@@ -0,0 +1,52 @@
+package utils;
+
+import model.Player;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.XMLConstants;
+import org.xml.sax.SAXException;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+public class XMLReader {
+
+    public static Document convertStringToDocument(String xmlString) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        return builder.parse(new ByteArrayInputStream(xmlString.getBytes()));
+    }
+
+    public static boolean validateXML(Document xmlDoc, String xsdPath) throws SAXException, IOException {
+        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = factory.newSchema(new StreamSource(new File(xsdPath)));
+        schema.newValidator().validate(new DOMSource(xmlDoc));
+        return true;
+    }
+
+    public static Player extractPlayerFromXML(Document xmlDoc) {
+        Element root = xmlDoc.getDocumentElement();
+        String nickname = getElementValue(root, "nickname");
+        String password = getElementValue(root, "password");
+        String nationality = getElementValue(root, "nationality");
+        int age = Integer.parseInt(getElementValue(root, "age"));
+        String photoUrl = getElementValue(root, "photoUrl");
+        return new Player(nickname, password, nationality, age, photoUrl);
+    }
+
+    private static String getElementValue(Element parent, String tagName) {
+        NodeList nodeList = parent.getElementsByTagName(tagName);
+        if (nodeList != null && nodeList.getLength() > 0) {
+            return nodeList.item(0).getTextContent();
+        }
+        return null;
+    }
+}
