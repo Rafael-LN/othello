b83a332563ce3d261b3c8b05cc5fb835673f3b30
Rafael-LN <rafaelnunes3092@gmail.com>
2024-06-04 21:40:54 +0100

    Remove some redundancy in XMLBuilder

--- src/utils/XMLBuilder.java
+++ src/utils/XMLBuilder.java
@@ -8,58 +8,62 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 public class XMLBuilder {
-    // Método para criar um documento XML de registo de jogador
+
     public static Document createPlayerRegistrationXML(Player player) throws Exception {
         Document doc = createDocument();
         Element rootElement = createRootElement(doc, "request", "type", "register");
 
-        // Adiciona elementos ao documento
-        appendPlayerDetails(doc, rootElement, player);
-
+        appendChildElements(doc, rootElement,
+                new String[]{"nickname", "password", "nationality", "age", "photo"},
+                new String[]{player.getNickname(), player.getPassword(), player.getNationality(),
+                        String.valueOf(player.getAge()), player.getBase64Photo()});
         return doc;
     }
 
-    // Método para criar um documento XML de registro de jogadas
     public static Document createPlayXML(Player player, int row, int col) throws Exception {
         Document doc = createDocument();
         Element rootElement = createRootElement(doc, "game", null, null);
 
-        // Adiciona elementos ao documento
         Element users = doc.createElement("players");
         rootElement.appendChild(users);
 
         Element user = doc.createElement("player");
         users.appendChild(user);
-        user.appendChild(createElement(doc, "nickname", player.getNickname()));
-        user.appendChild(createElement(doc, "color", "BLACK"));
+
+        appendChildElements(doc, user,
+                new String[]{"nickname", "color"},
+                new String[]{player.getNickname(), "BLACK"});
 
         Element move = doc.createElement("move");
         rootElement.appendChild(move);
-        move.appendChild(createElement(doc, "nickname", player.getNickname()));
+        appendChildElements(doc, move, new String[]{"nickname"}, new String[]{player.getNickname()});
 
         Element position = doc.createElement("position");
         rootElement.appendChild(position);
-        position.appendChild(createElement(doc, "row", String.valueOf(row)));
-        position.appendChild(createElement(doc, "col", String.valueOf(col)));
+        appendChildElements(doc, position, new String[]{"row", "col"},
+                new String[]{String.valueOf(row), String.valueOf(col)});
 
         return doc;
     }
 
-    // Método para criar elementos XML
+    private static void appendChildElements(Document doc, Element parent, String[] tagNames, String[] textContents) {
+        for (int i = 0; i < tagNames.length; i++) {
+            parent.appendChild(createElement(doc, tagNames[i], textContents[i]));
+        }
+    }
+
     private static Element createElement(Document doc, String tagName, String textContent) {
         Element element = doc.createElement(tagName);
         element.appendChild(doc.createTextNode(textContent));
         return element;
     }
 
-    // Método para criar o documento XML
     private static Document createDocument() throws Exception {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         return dBuilder.newDocument();
     }
 
-    // Método para criar o elemento raiz
     private static Element createRootElement(Document doc, String rootName, String attributeName, String attributeValue) {
         Element rootElement = doc.createElement(rootName);
         if (attributeName != null && attributeValue != null) {
@@ -67,14 +71,5 @@ public class XMLBuilder {
         }
         doc.appendChild(rootElement);
         return rootElement;
-    }
-
-    // Método para adicionar detalhes do jogador
-    private static void appendPlayerDetails(Document doc, Element parent, Player player) {
-        parent.appendChild(createElement(doc, "nickname", player.getNickname()));
-        parent.appendChild(createElement(doc, "password", player.getPassword()));
-        parent.appendChild(createElement(doc, "nationality", player.getNationality()));
-        parent.appendChild(createElement(doc, "age", String.valueOf(player.getAge())));
-        parent.appendChild(createElement(doc, "photo", player.getBase64Photo()));
     }
 }
